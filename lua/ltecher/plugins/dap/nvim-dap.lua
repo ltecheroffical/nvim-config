21319e0abfc0fd6e414aea362881df7b6a78a1d8
LTecher Offical <82725076+ltecheroffical@users.noreply.github.com>
2024-05-20 09:07:39 -0400

    Remove project LLDB

--- lua/ltecher/plugins/dap/nvim-dap.lua
+++ lua/ltecher/plugins/dap/nvim-dap.lua
@@ -5,39 +5,6 @@ return {
     local dap = require('dap')
     local keymap = vim.keymap
 
-    local sourceFileMap = {}
-
-    -- Read from it
-    local file = io.open(vim.fn.getcwd() .. "/dap_source_map.txt", "r")
-    if file then
-      -- Read the lines
-      for line in file:lines() do
-        -- Replace "${cwd}" with the cwd
-        line = line:gsub("${cwd}", vim.fn.getcwd())
-
-        -- Split between :
-        local split = vim.split(line, ":")
-        -- Get the frst part as src and the second as dest
-        sourceFileMap[split[1]] = split[2]
-      end
-    end
-
-    dap.configurations.cpp = {
-      {
-        name = "Project: Launch",
-        type = "codelldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false,
-        args = {},
-        sourceLanguages = { "cpp" },
-        sourceFileMap = sourceFileMap
-      }
-    }
-
     keymap.set("n", "<leader>dbb", function()
         require("dap").toggle_breakpoint()
       end,
