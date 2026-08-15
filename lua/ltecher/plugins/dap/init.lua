330c561782e98374444bd9462604ab9e74fe4f20
LTecher Offical <ltecheroffical@gmail.com>
2026-08-14 22:23:32 -0400

    remove reduantant remote GDB config

--- lua/ltecher/plugins/dap/init.lua
+++ lua/ltecher/plugins/dap/init.lua
@@ -21,29 +21,6 @@ return {
 			local keymap = vim.keymap
 			local dap = require("dap")
 
-			local remote_gdb_config = {
-				name = "GDB Remote",
-				type = "cppdbg",
-				request = "launch",
-				cwd = "${workspaceFolder}",
-				program = function()
-					return vim.fn.input("Program: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				MIMode = "gdb",
-				miDebuggerServerAddress = function()
-					return vim.fn.input("Server: ", "localhost:1234")
-				end
-
-			}
-
-			dap.configurations.c = {
-				remote_gdb_config,
-			}
-
-			dap.configurations.cpp = {
-				remote_gdb_config,
-			}
-
 			keymap.set("n", "<leader>dbb", function()
 					dap.toggle_breakpoint()
 				end,
