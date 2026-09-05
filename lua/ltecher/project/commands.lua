7b830186ca5b19e68f9d530d7b64802b80b05f35
LTecher Offical <ltecheroffical@gmail.com>
2026-09-05 14:55:42 -0400

    improve project run and project build

--- lua/ltecher/project/commands.lua
+++ lua/ltecher/project/commands.lua
@@ -1,48 +1,48 @@
-local function show_terminal()
-    local terminal_buffers = vim.tbl_filter(function(buf)
-        return vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_get_option(buf, "buftype") == "terminal"
-    end, vim.api.nvim_list_bufs())
-
-    if #terminal_buffers == 0 then
-        vim.cmd('below split')
-        vim.cmd('terminal')
-        return
-    else
-        if vim.fn.getbufinfo(terminal_buffers[1])[1].hidden == 1 then
-            vim.cmd('below split')
-            vim.api.nvim_set_current_buf(terminal_buffers[1])
-        else
-            vim.api.nvim_set_current_win(vim.fn.getbufinfo(terminal_buffers[1])[1].windows[1])
-        end
-    end
+local function get_finished_terminal()
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_valid(buf)
+			and vim.bo[buf].buftype == "terminal"
+		then
+			local job_id = vim.b[buf].terminal_job_id
+			if job_id == nil or vim.fn.jobwait({ job_id }, 0)[1] ~= -1 then
+				return buf
+			end
+		end
+	end
+	return nil
 end
 
-vim.api.nvim_create_user_command("ProjectBuild", function ()
-    if io.open(vim.fn.getcwd() .. "/build_project.sh") == nil then
-        print("No build_project.sh found in this directory")
-        return
-    end
+local function run_in_terminal(command)
+	local dead_buf = get_finished_terminal()
 
-    show_terminal()
+	if dead_buf then
+		local info = vim.fn.getbufinfo(dead_buf)[1]
+		if #info.windows > 0 then
+			vim.api.nvim_set_current_win(info.windows[1])
+		else
+			vim.cmd('below split')
+			vim.api.nvim_set_current_buf(dead_buf)
+		end
+		vim.cmd('enew')
+		vim.cmd('term ' .. command)
+		vim.cmd('bdelete! ' .. dead_buf)
+	else
+		vim.cmd('below split | term ' .. command)
+	end
+end
 
-    vim.fn.feedkeys('a')
-    local enter = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
+vim.api.nvim_create_user_command("ProjectBuild", function()
+	if io.open(vim.fn.getcwd() .. "/build_project.sh") == nil then
+		print("No build_project.sh found in this directory")
+		return
+	end
+	run_in_terminal('/bin/sh build_project.sh')
+end, { desc = "Build the project as specified by build_project.sh" })
 
-    vim.fn.feedkeys("/bin/sh build_project.sh")
-    vim.fn.feedkeys(enter)
-end, {desc="Build the project as specified by build_project.sh"})
-
-vim.api.nvim_create_user_command("ProjectRun", function ()
-    if io.open(vim.fn.getcwd() .. "/run_project.sh") == nil then
-        print("No run_project.sh found in this directory")
-        return
-    end
-
-    show_terminal()
-
-    vim.fn.feedkeys('a')
-    local enter = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
-
-    vim.fn.feedkeys("/bin/sh run_project.sh")
-    vim.fn.feedkeys(enter)
-end, {desc="Run the project as specified by run_project.sh"})
+vim.api.nvim_create_user_command("ProjectRun", function()
+	if io.open(vim.fn.getcwd() .. "/run_project.sh") == nil then
+		print("No run_project.sh found in this directory")
+		return
+	end
+	run_in_terminal('/bin/sh run_project.sh')
+end, { desc = "Run the project as specified by run_project.sh" })
